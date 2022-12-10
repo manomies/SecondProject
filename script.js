97a1f7aba72cb13d5277c11b620ead23a5606545
manomies <manomingeo@gmail.com>
2022-12-11 01:32:54 +0400

    lesson2

--- script.js
+++ script.js
@@ -9,18 +9,41 @@ const goods = [
 
   ];
   
-  const renderGoodsItem = (title='carpet', price='100', image) => {
-    return `
-      <div class="goods-item">
-        <h3 class="goods-item-text">${title}</h3>
-        <p class="goods-item-price">${price}</p>
-        <button class="goods-item-button"><image class="goods-item-image" src="${image}"></image></button>
-        </div>`;
-  };
+class GoodsItem {
+    constructor ({title = 'Carpet', price = 100, image}) {
+        this.title = title;
+        this.price = price;
+        this.image = image;
+    }
+    render () {
+        return `
+        <div class="goods-item">
+          <h3 class="goods-item-text">${this.title}</h3>
+          <p class="goods-item-price">${this.price}</p>
+          <button class="goods-item-button"><image class="goods-item-image" src="${this.image}"></image></button>
+          </div>`;
+    }
+}
+
+class GoodsList {
+    items = [];
+    fetchGoods () {
+        this.items = goods;
+    }
+        calculatePrice() {
+            return this.items.reduce((prev, { price }) => {
+              return prev + price;
+            }, 0)
+          }
+    render () {
+        const goods = this.items.map(item => {
+            const goodItem = new GoodsItem(item);
+            return goodItem.render()
+        }).join('')
+        document.querySelector('.goods-list').innerHTML = goods;
+    }
+}
   
-  const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.image));
-    document.querySelector('.goods-list').innerHTML = goodsList;
-  }
-  
-  renderGoodsList(goods);
+const goodsList = new GoodsList;
+goodsList.fetchGoods();
+goodsList.render();
